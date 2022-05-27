9ede140b4166640ec6530fe234602b4de82ec647
gaverianov <gregory.averianov@gmail.com>
2022-05-27 16:29:47 +0300

    implementation of PreScoringServiceImpl

--- conveyor/src/main/java/ru/neoflex/credit/conveyor/service/impl/PreScoringServiceImpl.java
+++ conveyor/src/main/java/ru/neoflex/credit/conveyor/service/impl/PreScoringServiceImpl.java
@@ -1,30 +1,45 @@
 package ru.neoflex.credit.conveyor.service.impl;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.neoflex.credit.conveyor.model.LoanApplicationRequestDTO;
 import ru.neoflex.credit.conveyor.model.LoanOfferDTO;
+import ru.neoflex.credit.conveyor.service.abstracts.LoanCalculatorService;
 import ru.neoflex.credit.conveyor.service.abstracts.PreScoringService;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class PreScoringServiceImpl implements PreScoringService {
 
-    //sort list by parameter rate ASC
+    private final LoanCalculatorService loanCalculator;
     @Override
     public List<LoanOfferDTO> createListOffers(LoanApplicationRequestDTO loanApplicationRequestDTO) {
-        return List.of(
-                createOffer(true, true, loanApplicationRequestDTO),
-                createOffer(true, false, loanApplicationRequestDTO),
-                createOffer(false, true, loanApplicationRequestDTO),
-                createOffer(false, false, loanApplicationRequestDTO)
-        );
+        List<LoanOfferDTO> listOffers = new ArrayList<>();
+
+        listOffers.add(createOffer(true, true, loanApplicationRequestDTO));
+        listOffers.add(createOffer(true, false, loanApplicationRequestDTO));
+        listOffers.add(createOffer(false, true, loanApplicationRequestDTO));
+        listOffers.add(createOffer(false, false, loanApplicationRequestDTO));
+
+        listOffers.sort((o1, o2) -> o1.getRate().compareTo(o2.getRate()));
+        return listOffers;
     }
 
-
     private LoanOfferDTO createOffer(boolean isInsuranceEnabled, boolean isSalaryClient, LoanApplicationRequestDTO loanApplicationRequestDTO) {
-        return null;
+        BigDecimal totalAmount = loanCalculator.calculateTotalAmount(loanApplicationRequestDTO.getAmount(), isInsuranceEnabled);
+        BigDecimal rate = loanCalculator.calculateRate(isInsuranceEnabled, isSalaryClient);
+        BigDecimal monthlyPayment = loanCalculator.calculateMonthlyPayment(totalAmount, loanApplicationRequestDTO.getTerm(), rate);
+        return new LoanOfferDTO()
+                .requestedAmount(loanApplicationRequestDTO.getAmount())
+                .totalAmount(totalAmount)
+                .term(loanApplicationRequestDTO.getTerm())
+                .monthlyPayment(monthlyPayment)
+                .rate(rate)
+                .isInsuranceEnabled(isInsuranceEnabled)
+                .isSalaryClient(isSalaryClient);
     }
 }
