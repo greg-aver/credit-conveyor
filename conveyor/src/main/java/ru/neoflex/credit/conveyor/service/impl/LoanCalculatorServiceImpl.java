76d500f046c92f4f52c1d72572ab0a2ee7d40993
gaverianov <gregory.averianov@gmail.com>
2022-06-01 12:17:55 +0300

    add unit tests. Coverage lines 87%

--- conveyor/src/main/java/ru/neoflex/credit/conveyor/service/impl/LoanCalculatorServiceImpl.java
+++ conveyor/src/main/java/ru/neoflex/credit/conveyor/service/impl/LoanCalculatorServiceImpl.java
@@ -10,7 +10,7 @@ import ru.neoflex.credit.conveyor.model.EmploymentDTO;
 import ru.neoflex.credit.conveyor.model.PaymentScheduleElement;
 import ru.neoflex.credit.conveyor.model.ScoringDataDTO;
 import ru.neoflex.credit.conveyor.service.abstracts.LoanCalculatorService;
-import ru.neoflex.credit.conveyor.service.abstracts.factory.FactoryPaymentScheduleElement;
+import ru.neoflex.credit.conveyor.service.impl.factory.PaymentScheduleElementFactory;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -19,13 +19,17 @@ import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static java.math.RoundingMode.CEILING;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LoanCalculatorServiceImpl implements LoanCalculatorService {
     @Value("${properties.baseRate}")
     private final String BASE_RATE;
-    private BigDecimal CURRENT_RATE = new BigDecimal(BASE_RATE);
+    @Value("${properties.baseRate}")
+    private BigDecimal CURRENT_RATE/* = new BigDecimal(BASE_RATE)*/;
     @Value("${properties.baseInsurance}")
     private final String BASE_INSURANCE;
     @Value("${properties.percentageInsurance}")
@@ -35,14 +39,22 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
     @Value("${properties.rateDiscountInsuranceEnabled}")
     private final String RATE_DISCOUNT_INSURANCE_ENABLED;
 
-    private final FactoryPaymentScheduleElement factoryPayment;
     @Override
     public BigDecimal calculateTotalAmount(BigDecimal amount, boolean isInsuranceEnabled) {
+        log.info("Start calculating the total amount insurance included or not");
+        log.info("totalAmount = {}", amount);
         if (isInsuranceEnabled) {
+            log.debug("Insurance included");
             BigDecimal insuranceCost = new BigDecimal(BigInteger.ZERO);
-            insuranceCost.add(amount.multiply(new BigDecimal(PERCENTAGE_INSURANCE)));
-            amount.add(insuranceCost);
+
+            insuranceCost = insuranceCost.add(amount.multiply(new BigDecimal(PERCENTAGE_INSURANCE)));
+
+            log.debug("insuranceCost = {}", insuranceCost);
+            amount = amount.add(insuranceCost);
+        } else {
+            log.debug("Insurance NOT included");
         }
+        log.info("End calculate total amount. Amount = {}", amount);
         return amount;
     }
 
@@ -54,11 +66,11 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
         log.debug("currentRate = {}", currentRate);
 
         if (isInsuranceEnabled) {
-            currentRate.subtract(new BigDecimal(RATE_DISCOUNT_INSURANCE_ENABLED));
+            currentRate = currentRate.subtract(new BigDecimal(RATE_DISCOUNT_INSURANCE_ENABLED));
             log.debug("Insurance enabled. Rate downgrade by {}", RATE_DISCOUNT_INSURANCE_ENABLED);
         }
         if (isSalaryClient) {
-            currentRate.subtract(new BigDecimal(RATE_DISCOUNT_SALARY_CLIENT));
+            currentRate = currentRate.subtract(new BigDecimal(RATE_DISCOUNT_SALARY_CLIENT));
             log.debug("isSalaryClient = true. Rate downgrade by {}", RATE_DISCOUNT_SALARY_CLIENT);
         }
         CURRENT_RATE = currentRate;
@@ -67,20 +79,22 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
     }
 
     /*
-    * Ежемесячный платеж = Коэффициент аннуитета * Сумма кредита
-    * Коэффициент = (i * (1 + i)^n) / ((1 + i)^n - 1)
-    * i - процентная ставка по кредиту в месяц
-    * i = годовая ставка / 12 месяцев
-    * n - количество месяцев, за которые нужно погасить кредит
-    **/
+     * Ежемесячный платеж = Коэффициент аннуитета * Сумма кредита
+     * Коэффициент = (i * (1 + i)^n) / ((1 + i)^n - 1)
+     * i - процентная ставка по кредиту в месяц
+     * i = годовая ставка / 12 месяцев
+     * n - количество месяцев, за которые нужно погасить кредит
+     **/
     @Override
     public BigDecimal calculateMonthlyPayment(BigDecimal totalAmount, Integer term, BigDecimal rate) {
         log.info("Start calculate monthly payment");
+        log.info("totalAmount = {}, term = {}, rate = {}", totalAmount, term, rate);
 
-        BigDecimal rateMonthly = rate.divide(BigDecimal.valueOf(12));
+        BigDecimal rateMonthly = rate.divide(BigDecimal.valueOf(12), 10, CEILING)
+                .divide(BigDecimal.valueOf(100), 10, CEILING);
         log.debug("rateMonthly = {}", rateMonthly);
 
-    //  intermediateNumber = (1 + i)^n
+        //  intermediateNumber = (1 + i)^n
         BigDecimal intermediateNumber = rateMonthly.add(BigDecimal.ONE).pow(term);
         log.debug("intermediateNumber = {}", intermediateNumber);
 
@@ -90,7 +104,10 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
         BigDecimal denominator = intermediateNumber.subtract(BigDecimal.ONE);
         log.debug("denominator = {}", denominator);
 
-        BigDecimal result = numerator.divide(denominator).multiply(totalAmount);
+        BigDecimal annuityRatio = numerator.divide(denominator, 2, CEILING);
+        log.debug("annuity ratio = {}", annuityRatio);
+
+        BigDecimal result = annuityRatio.multiply(totalAmount).setScale(2, CEILING);
         log.info("End calculate monthly payment. Result = {}", result);
 
         return result;
@@ -105,7 +122,7 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
         BigDecimal totalAmount = calculateTotalAmount(requestedAmount, isInsuranceEnabled);
         Integer term = scoringData.getTerm();
         BigDecimal rate = calculateRate(isInsuranceEnabled, isSalaryClient);
-        BigDecimal monthlyPayment = calculateMonthlyPayment(totalAmount,term,rate);
+        BigDecimal monthlyPayment = calculateMonthlyPayment(totalAmount, term, rate);
         List<PaymentScheduleElement> paymentScheduleElementList = calculateListPaymentSchedule(totalAmount, term, monthlyPayment, rate);
         BigDecimal psk = calculatePSK(requestedAmount, term, paymentScheduleElementList);
         return new CreditDTO()
@@ -124,27 +141,28 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
     ) {
         ArrayList<PaymentScheduleElement> resultList = new ArrayList<>();
         LocalDate datePayment = LocalDate.now();
-        BigDecimal remainingDebt = totalAmount.setScale(2);
+        BigDecimal remainingDebt = totalAmount.setScale(2, CEILING);
 
         for (int i = 1; i <= term; i++) {
 
-            BigDecimal interestPayment = calculateInterestPayment(remainingDebt);
-            BigDecimal debtPayment = monthlyPayment.subtract(interestPayment);
-            datePayment.plusMonths(1L);
-            remainingDebt = remainingDebt.subtract(debtPayment);
-            factoryPayment.createPaymentScheduleElement(
+            BigDecimal interestPayment = calculateInterestPayment(remainingDebt).setScale(2, CEILING);
+            BigDecimal debtPayment = monthlyPayment.subtract(interestPayment).setScale(2, CEILING);
+            datePayment = datePayment.plusMonths(1L);
+            remainingDebt = remainingDebt.subtract(debtPayment).setScale(2, CEILING);
+            PaymentScheduleElement paymentScheduleElement = PaymentScheduleElementFactory.createPaymentScheduleElement(
                     i, datePayment, monthlyPayment, interestPayment, debtPayment, remainingDebt
             );
+            resultList.add(paymentScheduleElement);
         }
         return resultList;
     }
 
     /* Формула рассчета полной стоимости кредита:
-    * ПСК = (S/So - 1) / (100 * n)
-    * n - срок погашения в годах
-    * S - сумма всех кредитных платежей
-    * So - сумма полученная от банка
-    * */
+     * ПСК = (S/So - 1) / (100 * n)
+     * n - срок погашения в годах
+     * S - сумма всех кредитных платежей
+     * So - сумма полученная от банка
+     * */
 
     private BigDecimal calculatePSK(
             BigDecimal requestedAmount, Integer term, List<PaymentScheduleElement> paymentSchedule
@@ -152,32 +170,31 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
         log.info("Start calculate PSK");
 
         BigDecimal termYears = new BigDecimal(term)
-                .divide(BigDecimal.valueOf(12))
-                .setScale(2);
+                .divide(BigDecimal.valueOf(12), 2, CEILING);
         log.debug("termYears = {}", termYears);
 
         BigDecimal paymentAmount = paymentSchedule
                 .stream()
                 .map(PaymentScheduleElement::getTotalPayment)
-                .reduce(BigDecimal::add).orElse(BigDecimal.ONE);
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
         log.debug("Payment amount = {}", paymentAmount);
 
-        BigDecimal numerator = paymentAmount.divide(requestedAmount)
+        BigDecimal numerator = paymentAmount.divide(requestedAmount, 2, CEILING)
                 .subtract(BigDecimal.ONE);
         BigDecimal psk = numerator
-                .divide(termYears)
-                .divide(BigDecimal.valueOf(100))
-                .divide(termYears)
-                .setScale(2);
+                .divide(termYears, 2, CEILING)
+                .divide(BigDecimal.valueOf(100), 2, CEILING)
+                .divide(termYears, 2, CEILING);
+//                .setScale(2);
         log.info("End calculate psk. PSK = {}", psk);
         return psk;
     }
 
     private BigDecimal calculateInterestPayment(BigDecimal remainingDebt) {
-        BigDecimal rateAbsolute = new BigDecimal("#.##");
-        rateAbsolute.add(CURRENT_RATE)
-                .divide(BigDecimal.valueOf(100));
-        return rateAbsolute.multiply(remainingDebt);
+        BigDecimal rateAbsolute = CURRENT_RATE
+                .divide(BigDecimal.valueOf(100), CEILING);
+        BigDecimal monthlyRate = rateAbsolute.divide(BigDecimal.valueOf(12), 10, CEILING);
+        return monthlyRate.multiply(remainingDebt).setScale(2, CEILING);
     }
 
     /* Правила скоринга:
@@ -202,22 +219,22 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
                 reasonsRefusal.add("Denied a loan: Client unemployed");
                 break;
             case SELF_EMPLOYED:
-                currentRate.add(BigDecimal.ONE);
+                currentRate = currentRate.add(BigDecimal.ONE);
                 log.debug("Client is self employed. Rate increased by 1");
                 break;
             case BUSINESS_OWNER:
-                currentRate.add(BigDecimal.valueOf(3));
+                currentRate = currentRate.add(BigDecimal.valueOf(3));
                 log.debug("Client is business owner. Rate increased by 3");
                 break;
         }
 
         switch (employment.getPosition()) {
             case MID_MANAGER:
-                currentRate.subtract(BigDecimal.valueOf(2));
+                currentRate = currentRate.subtract(BigDecimal.valueOf(2));
                 log.debug("Client is middle manager. Rate reduced by 3");
                 break;
             case TOP_MANAGER:
-                currentRate.subtract(BigDecimal.valueOf(4));
+                currentRate = currentRate.subtract(BigDecimal.valueOf(4));
                 log.debug("Client is top manager. Rate reduced by 4");
                 break;
         }
@@ -247,18 +264,18 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
 
         switch (scoringData.getGender()) {
             case NON_BINARY:
-                currentRate.add(BigDecimal.valueOf(3));
+                currentRate = currentRate.add(BigDecimal.valueOf(3));
                 log.debug("The client has a non-binary gender. Rate increased by 3");
                 break;
             case MALE:
                 if (clientAge >= 30 && clientAge <= 55) {
-                    currentRate.subtract(BigDecimal.valueOf(3));
+                    currentRate = currentRate.subtract(BigDecimal.valueOf(3));
                     log.debug("The client is male and between 30 and 55 years of age. Rate reduced 3");
                 }
                 break;
             case FEMALE:
                 if (clientAge >= 35 && clientAge <= 60) {
-                    currentRate.subtract(BigDecimal.valueOf(3));
+                    currentRate = currentRate.subtract(BigDecimal.valueOf(3));
                     log.debug("The client is female and between 35 and 60 years of age. Rate reduced 3");
                 }
                 break;
@@ -266,17 +283,17 @@ public class LoanCalculatorServiceImpl implements LoanCalculatorService {
 
         switch (scoringData.getMaritalStatus()) {
             case MARRIED:
-                currentRate.subtract(BigDecimal.valueOf(3));
+                currentRate = currentRate.subtract(BigDecimal.valueOf(3));
                 log.debug("The client is married. Rate reduced 3");
                 break;
             case DIVORCED:
-                currentRate.add(BigDecimal.ONE);
+                currentRate = currentRate.add(BigDecimal.ONE);
                 log.debug("The client is divorced. Rate increased 1");
                 break;
         }
 
         if (scoringData.getDependentAmount() > 1) {
-            currentRate.add(BigDecimal.ONE);
+            currentRate = currentRate.add(BigDecimal.ONE);
         }
 
         CURRENT_RATE = currentRate;
