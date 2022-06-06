813e5c5ef5599bbc5b975a90ef9301120153b527
gaverianov <gregory.averianov@gmail.com>
2022-06-06 16:51:32 +0300

    added method createClientByRequest

--- deal/src/main/java/ru/neoflex/credit/deal/service/impl/DealServiceImpl.java
+++ deal/src/main/java/ru/neoflex/credit/deal/service/impl/DealServiceImpl.java
@@ -1,15 +1,13 @@
 package ru.neoflex.credit.deal.service.impl;
 
-import ru.neoflex.credit.deal.model.LoanApplicationRequestDTO;
-import ru.neoflex.credit.deal.model.LoanOfferDTO;
-import ru.neoflex.credit.deal.model.ScoringDataDTO;
+import ru.neoflex.credit.deal.model.*;
 import ru.neoflex.credit.deal.service.abstracts.DealService;
 
 import java.util.List;
 
 public class DealServiceImpl  implements DealService {
     @Override
-    public List<LoanOfferDTO> createApplication(LoanApplicationRequestDTO loanApplicationRequestDTO) {
+    public List<LoanOfferDTO> createApplication(LoanApplicationRequestDTO request) {
         return null;
     }
 
@@ -21,5 +19,19 @@ public class DealServiceImpl  implements DealService {
     @Override
     public void calculateCredit(Long applicationId, ScoringDataDTO scoringDataDTO) {
 
+    }
+
+    private Client createClientByRequest(LoanApplicationRequestDTO request) {
+        Passport passportClient = new Passport()
+                .series(request.getPassportSeries())
+                .number(request.getPassportNumber());
+
+        return new Client()
+                .lastName(request.getLastName())
+                .firstName(request.getFirstName())
+                .middleName(request.getMiddleName())
+                .birthDate(request.getBirthdate())
+                .email(request.getEmail())
+                .passport(passportClient);
     }
 }
