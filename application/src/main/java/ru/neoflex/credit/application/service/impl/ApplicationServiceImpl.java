64c42ce055f7b1bf4d0cb8ad3a0ef975b1e67282
gaverianov <gregory.averianov@gmail.com>
2022-06-08 12:27:54 +0300

    added methods createApplication and offer in service

--- application/src/main/java/ru/neoflex/credit/application/service/impl/ApplicationServiceImpl.java
+++ application/src/main/java/ru/neoflex/credit/application/service/impl/ApplicationServiceImpl.java
@@ -18,12 +18,22 @@ public class ApplicationServiceImpl implements ApplicationService {
         dealFeignClient.offer(loanOfferDTO);
     }
 
-    private void preScoring(LoanApplicationRequestDTO request) {
-
-    }
     @Override
     public List<LoanOfferDTO> createApplication(LoanApplicationRequestDTO request) {
         preScoring(request);
-        return null;
+        List<LoanOfferDTO> loanOffersList = dealFeignClient.createApplication(request).getBody();
+        return loanOffersList;
+    }
+
+    /* Правила прескоринга (можно придумать новые правила или изменить существующие):
+
+    1. Имя, Фамилия - от 2 до 30 латинских букв. Отчество, при наличии - от 2 до 30 латинских букв.
+    2. Сумма кредита - действительно число, большее или равное 10000.
+    3. Срок кредита - целое число, большее или равное 6.
+    4. Дата рождения - число в формате гггг-мм-дд, не позднее 18 лет с текущего дня.
+    5. Email адрес - строка, подходящая под паттерн [\w\.]{2,50}@[\w\.]{2,20}
+    6. Серия паспорта - 4 цифры, номер паспорта - 6 цифр.**/
+    private void preScoring(LoanApplicationRequestDTO request) {
+
     }
 }
