1aa8804e0bff65f8a6a41a412a11fcb58943d096
gaverianov <gregory.averianov@gmail.com>
2022-06-08 12:52:06 +0300

    realized logic for pre scoring

--- application/src/main/java/ru/neoflex/credit/application/service/impl/ApplicationServiceImpl.java
+++ application/src/main/java/ru/neoflex/credit/application/service/impl/ApplicationServiceImpl.java
@@ -2,17 +2,24 @@ package ru.neoflex.credit.application.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.neoflex.credit.application.exception.PreScoringException;
 import ru.neoflex.credit.application.feign.DealFeignClient;
 import ru.neoflex.credit.application.service.abstracts.ApplicationService;
 import ru.neoflex.credit.deal.model.LoanApplicationRequestDTO;
 import ru.neoflex.credit.deal.model.LoanOfferDTO;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ApplicationServiceImpl implements ApplicationService {
     private final DealFeignClient dealFeignClient;
+
     @Override
     public void offer(LoanOfferDTO loanOfferDTO) {
         dealFeignClient.offer(loanOfferDTO);
@@ -34,6 +41,46 @@ public class ApplicationServiceImpl implements ApplicationService {
     5. Email адрес - строка, подходящая под паттерн [\w\.]{2,50}@[\w\.]{2,20}
     6. Серия паспорта - 4 цифры, номер паспорта - 6 цифр.**/
     private void preScoring(LoanApplicationRequestDTO request) {
+        ArrayList<String> reasonsRefusal = new ArrayList<>();
 
+        if (!request.getFirstName().matches("[A-Za-z]{2,30}")) {
+            reasonsRefusal.add("Incorrect first name by client");
+        }
+
+        if (!request.getLastName().matches("[A-Za-z]{2,30}")) {
+            reasonsRefusal.add("Incorrect last name by client");
+        }
+
+        if (request.getMiddleName() != null &&  request.getMiddleName().matches("[A-Za-z]{2,30}")) {
+            reasonsRefusal.add("Incorrect middle name by client");
+        }
+
+        if (request.getAmount().compareTo(BigDecimal.valueOf(10000)) < 0) {
+            reasonsRefusal.add("Amount < 10_000");
+        }
+
+        if (request.getTerm() < 6) {
+            reasonsRefusal.add("Term < 6");
+        }
+
+        if (Period.between(request.getBirthdate(), LocalDate.now()).getYears() < 18) {
+            reasonsRefusal.add("Client under 18 years");
+        }
+
+        if (!request.getEmail().matches("[\\w\\.]{2,50}@[\\w\\.]{2,20}")) {
+            reasonsRefusal.add("Invalid email format");
+        }
+
+        if (!request.getPassportSeries().matches("\\d{4}")) {
+            reasonsRefusal.add("Incorrect passport series");
+        }
+
+        if (!request.getPassportNumber().matches("\\d{6}")) {
+            reasonsRefusal.add("Incorrect passport number");
+        }
+
+        if (reasonsRefusal.size() > 0) {
+            throw new PreScoringException("Failed pre scoring: " + Arrays.deepToString(reasonsRefusal.toArray()));
+        }
     }
 }
