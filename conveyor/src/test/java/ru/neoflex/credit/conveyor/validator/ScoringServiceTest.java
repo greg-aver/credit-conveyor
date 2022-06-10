8d3f21c2f72afbf014ea19e5ad31996fda6a8d1d
gaverianov <gregory.averianov@gmail.com>
2022-06-10 13:23:55 +0300

    add tests for scoring service

--- conveyor/src/test/java/ru/neoflex/credit/conveyor/validator/ScoringServiceTest.java
+++ conveyor/src/test/java/ru/neoflex/credit/conveyor/validator/ScoringServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import ru.neoflex.credit.conveyor.exception.ScoringException;
 import ru.neoflex.credit.conveyor.model.EmploymentDTO;
 import ru.neoflex.credit.conveyor.model.ScoringDataDTO;
 
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ScoringServiceTest {
@@ -33,7 +35,7 @@ public class ScoringServiceTest {
                 .term(3)
                 .firstName("Mikhail")
                 .middleName("Alexey")
-                .lastName("Deev")
+                .lastName("Deyev")
                 .gender(ScoringDataDTO.GenderEnum.MALE)
                 .birthdate(LocalDate.of(2000, 6, 4))
                 .passportSeries("0808")
@@ -48,6 +50,38 @@ public class ScoringServiceTest {
                 .isSalaryClient(true);
 
         assertEquals(BigDecimal.valueOf(11), validator.scoring(scoringDataDTO, baseRate));
+    }
+
+    @Test
+    public void scoring_DataFail() {
+        String baseRate = "11";
+        EmploymentDTO employment = new EmploymentDTO()
+                .employmentStatus(EmploymentDTO.EmploymentStatusEnum.UNEMPLOYED)
+                .employerINN("00085866")
+                .salary(new BigDecimal("600"))
+                .workExperienceTotal(1)
+                .workExperienceCurrent(1);
+
+        ScoringDataDTO scoringDataDTO = new ScoringDataDTO()
+                .amount(BigDecimal.valueOf(9000000))
+                .term(3)
+                .firstName("M")
+                .middleName("A")
+                .lastName("D")
+                .gender(ScoringDataDTO.GenderEnum.MALE)
+                .birthdate(LocalDate.of(2015, 6, 4))
+                .passportSeries("0808")
+                .passportNumber("010203")
+                .passportIssueDate(LocalDate.of(2019, 6, 4))
+                .passportIssueBranch("Hospitable department of Dagestan")
+                .maritalStatus(ScoringDataDTO.MaritalStatusEnum.SINGLE)
+                .dependentAmount(900000)
+                .employment(employment)
+                .account("003456")
+                .isInsuranceEnabled(true)
+                .isSalaryClient(true);
+
+        assertThrows(ScoringException.class, () -> validator.scoring(scoringDataDTO, baseRate));
     }
 
 }
