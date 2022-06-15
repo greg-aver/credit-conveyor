615c36f11fa5de59d2245fde047773939d66e739
gaverianov <gregory.averianov@gmail.com>
2022-06-15 10:59:55 +0300

    added inappropriate status exception

---
+++ deal/src/main/java/ru/neoflex/credit/deal/exception/InappropriateStatusException.java
@@ -0,0 +1,8 @@
+package ru.neoflex.credit.deal.exception;
+
+public class InappropriateStatusException extends RuntimeException {
+
+    public InappropriateStatusException(String message) {
+        super(message);
+    }
+}
--- deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
+++ deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
@@ -1,11 +1,26 @@
 package ru.neoflex.credit.deal.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import ru.neoflex.credit.deal.model.Application;
+import ru.neoflex.credit.deal.repository.ApplicationRepository;
 import ru.neoflex.credit.deal.service.abstracts.MessageService;
 
+import javax.persistence.EntityExistsException;
+
+import static ru.neoflex.credit.deal.model.ApplicationStatusEnum.CC_APPROVED;
+
+@RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
+    private final ApplicationRepository applicationRepository;
     @Override
     public void send(Long applicationId) {
-
+        Application application = applicationRepository.getReferenceById(applicationId);
+        if (application == null) {
+            throw new EntityExistsException(String.format("Application with applicationID = %d not exists", applicationId));
+        }
+        if (application.status() != CC_APPROVED) {
+            throw
+        }
     }
 
     @Override
