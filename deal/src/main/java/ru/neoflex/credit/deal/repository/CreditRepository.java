ddd3c2a31a2b7f51346331c3e130a3a16ac92fe4
gaverianov <gregory.averianov@gmail.com>
2022-06-16 13:45:58 +0300

    added method code in service

--- deal/src/main/java/ru/neoflex/credit/deal/repository/CreditRepository.java
+++ deal/src/main/java/ru/neoflex/credit/deal/repository/CreditRepository.java
@@ -1,9 +1,9 @@
 package ru.neoflex.credit.deal.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.neoflex.credit.deal.model.Credit;
 
 @Repository
-public interface CreditRepository extends CrudRepository<Credit, Long> {
+public interface CreditRepository extends JpaRepository<Credit, Long> {
 }
--- deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
+++ deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
@@ -3,33 +3,33 @@ package ru.neoflex.credit.deal.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ru.neoflex.credit.deal.exception.InappropriateStatusException;
-import ru.neoflex.credit.deal.model.Application;
-import ru.neoflex.credit.deal.model.ApplicationStatusEnum;
-import ru.neoflex.credit.deal.model.ApplicationStatusHistoryDTO;
-import ru.neoflex.credit.deal.model.EmailMessage;
+import ru.neoflex.credit.deal.exception.SesDifferentException;
+import ru.neoflex.credit.deal.model.*;
 import ru.neoflex.credit.deal.repository.ApplicationRepository;
+import ru.neoflex.credit.deal.repository.CreditRepository;
 import ru.neoflex.credit.deal.service.abstracts.DossierService;
 import ru.neoflex.credit.deal.service.abstracts.MessageService;
 
-import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static ru.neoflex.credit.deal.model.ApplicationStatusEnum.*;
 import static ru.neoflex.credit.deal.model.ApplicationStatusHistoryDTO.ChangeTypeEnum.AUTOMATIC;
-import static ru.neoflex.credit.deal.model.EmailMessage.ThemeEnum.COMPLETE_DOCUMENT;
-import static ru.neoflex.credit.deal.model.EmailMessage.ThemeEnum.LINK_SIGN;
+import static ru.neoflex.credit.deal.model.CreditStatus.ISSUED;
+import static ru.neoflex.credit.deal.model.EmailMessage.ThemeEnum.*;
 
 @RequiredArgsConstructor
 @Slf4j
 public class MessageServiceImpl implements MessageService {
     private final ApplicationRepository applicationRepository;
+    private final CreditRepository creditRepository;
     private final DossierService dossierService;
     @Override
     public void send(Long applicationId) {
-        Application application = applicationRepository.getReferenceById(applicationId);
-        validApplication(application, CC_APPROVED);
-        application.status();
+        Application application = validApplication(applicationId, CC_APPROVED);
+        application.status(PREPARE_DOCUMENTS);
         application.statusHistory().add(
                 new ApplicationStatusHistoryDTO()
                         .time(LocalDateTime.now())
@@ -44,16 +44,20 @@ public class MessageServiceImpl implements MessageService {
                 .applicationId(applicationId));
     }
 
-    private void validApplication(Application application, ApplicationStatusEnum statusExpected) {
-        if (application == null) {
-            throw new EntityExistsException(String.format("Application with applicationID = %d not exists", application.id()));
-        }
+    private Application validApplication(Long applicationId, ApplicationStatusEnum statusExpected) {
+        Application application = applicationRepository.findById(applicationId).orElseThrow(() -> new EntityNotFoundException(String.format("Application with applicationID = %d not exists", applicationId)));
         ApplicationStatusEnum applicationStatus = application.status();
         if (applicationStatus != statusExpected) {
             throw new InappropriateStatusException(
                     String.format("Application with applicationID = %d has status = %s. Application status should be %s" , application.id(), applicationStatus, statusExpected)
             );
         }
+        return application;
+    }
+
+    private Application validApplication(Long applicationId) {
+        return applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Application with applicationID = %d not exists", applicationId)));
     }
 
     private int generateSes() {
@@ -61,8 +65,7 @@ public class MessageServiceImpl implements MessageService {
     }
     @Override
     public void sign(Long applicationId) {
-        Application application = applicationRepository.getReferenceById(applicationId);
-        validApplication(application, DOCUMENT_CREATED);
+        Application application = validApplication(applicationId, DOCUMENT_CREATED);
         int ses = generateSes();
         application.sesCode(ses);
         applicationRepository.save(application);
@@ -77,9 +80,45 @@ public class MessageServiceImpl implements MessageService {
     }
 
     @Override
-    public void code(Long applicationId, Integer ses) {
-        Application application = applicationRepository.getReferenceById(applicationId);
-        validApplication(application, DOCUMENT_CREATED);
+    public void code(Long applicationId, Integer sesActual) {
+        Application application = validApplication(applicationId, DOCUMENT_CREATED);
+        Integer sesExpected = application.sesCode();
+        if (!sesExpected.equals(sesActual)) {
+            throw new SesDifferentException(
+                    String.format("Expected ses code = %d, but ses actual = %d. ApplicationId = %d", sesExpected, sesActual, applicationId)
+            );
+        }
 
+        application.status(DOCUMENT_SIGNED);
+        application.statusHistory().add(new ApplicationStatusHistoryDTO()
+                .status(DOCUMENT_SIGNED)
+                .time(LocalDateTime.now())
+                .changeType(AUTOMATIC));
+
+        applicationRepository.save(application.signDate(LocalDate.now()));
+
+        issueCredit(applicationId);
+    }
+
+    private void issueCredit(Long applicationId) {
+        Application application = validApplication(applicationId);
+        Long creditId = application.credit().getId();
+
+        Credit credit = creditRepository.getReferenceById(creditId);
+
+        application.statusHistory().add(new ApplicationStatusHistoryDTO()
+                .status(CREDIT_ISSUED)
+                .time(LocalDateTime.now())
+                .changeType(ApplicationStatusHistoryDTO.ChangeTypeEnum.AUTOMATIC));
+
+        application.status(CREDIT_ISSUED);
+        applicationRepository.save(application);
+
+        creditRepository.save(credit.setCreditStatus(ISSUED));
+
+        dossierService.send(new EmailMessage()
+                .theme(CREDIT_ISSUE)
+                .applicationId(applicationId)
+                .address(application.client().getEmail()));
     }
 }
