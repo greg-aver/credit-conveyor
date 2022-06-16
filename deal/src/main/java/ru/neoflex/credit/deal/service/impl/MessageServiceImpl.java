576f29aa8dd68a5505dbd6ccb282298bd5134c3e
gaverianov <gregory.averianov@gmail.com>
2022-06-16 11:29:21 +0300

    added sign in dossier service

--- deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
+++ deal/src/main/java/ru/neoflex/credit/deal/service/impl/MessageServiceImpl.java
@@ -13,11 +13,12 @@ import ru.neoflex.credit.deal.service.abstracts.MessageService;
 
 import javax.persistence.EntityExistsException;
 import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
-import static ru.neoflex.credit.deal.model.ApplicationStatusEnum.CC_APPROVED;
-import static ru.neoflex.credit.deal.model.ApplicationStatusEnum.PREPARE_DOCUMENTS;
+import static ru.neoflex.credit.deal.model.ApplicationStatusEnum.*;
 import static ru.neoflex.credit.deal.model.ApplicationStatusHistoryDTO.ChangeTypeEnum.AUTOMATIC;
 import static ru.neoflex.credit.deal.model.EmailMessage.ThemeEnum.COMPLETE_DOCUMENT;
+import static ru.neoflex.credit.deal.model.EmailMessage.ThemeEnum.LINK_SIGN;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -27,16 +28,7 @@ public class MessageServiceImpl implements MessageService {
     @Override
     public void send(Long applicationId) {
         Application application = applicationRepository.getReferenceById(applicationId);
-        if (application == null) {
-            throw new EntityExistsException(String.format("Application with applicationID = %d not exists", applicationId));
-        }
-        ApplicationStatusEnum applicationStatus = application.status();
-        if (applicationStatus != CC_APPROVED) {
-            throw new InappropriateStatusException(
-                    String.format("Application with applicationID = %d has status = %s. Application status should be %s" , applicationId, applicationStatus, CC_APPROVED)
-            );
-        }
-
+        validApplication(application, CC_APPROVED);
         application.status();
         application.statusHistory().add(
                 new ApplicationStatusHistoryDTO()
@@ -52,9 +44,36 @@ public class MessageServiceImpl implements MessageService {
                 .applicationId(applicationId));
     }
 
+    private void validApplication(Application application, ApplicationStatusEnum statusExpected) {
+        if (application == null) {
+            throw new EntityExistsException(String.format("Application with applicationID = %d not exists", application.id()));
+        }
+        ApplicationStatusEnum applicationStatus = application.status();
+        if (applicationStatus != statusExpected) {
+            throw new InappropriateStatusException(
+                    String.format("Application with applicationID = %d has status = %s. Application status should be %s" , application.id(), applicationStatus, statusExpected)
+            );
+        }
+    }
+
+    private int generateSes() {
+        return ThreadLocalRandom.current().nextInt(1000, 10000);
+    }
     @Override
     public void sign(Long applicationId) {
-
+        Application application = applicationRepository.getReferenceById(applicationId);
+        validApplication(application, DOCUMENT_CREATED);
+        int ses = generateSes();
+        application.sesCode(ses);
+        applicationRepository.save(application);
+        String emailAddress = application.client().getEmail();
+        log.info(
+                String.format("Sign document. Application = %s with email %s", application, emailAddress)
+        );
+        dossierService.send(new EmailMessage()
+                .applicationId(applicationId)
+                .address(emailAddress)
+                .theme(LINK_SIGN));
     }
 
     @Override
